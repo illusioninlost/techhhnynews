d4df9e7fa1fcfec7c21d6d0ef87b613db5d627d9
Richard Chang <rchang9999@yahoo.com>
2017-12-21 04:52:38 +0000

    started scraping

--- lib/techhhynews/news.rb
+++ lib/techhhynews/news.rb
@@ -1,6 +1,16 @@
 class Techhhynews::News
-  attr_accessor :url, :title
-
+  attr_accessor :url, :title, :summary
   @@all = []
 
+  def initialize(title=nil, url=nil, summary=nil)
+    @title = title
+    @url = url
+    @summary = summary
+  end
+
+  def self.scrap_news
+  end
+
+  def doc
+  end
 end
