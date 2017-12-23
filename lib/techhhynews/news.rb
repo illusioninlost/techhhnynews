139923cddbb276c34d01f487c058ecfd796fe752
Richard Chang <rchang9999@yahoo.com>
2017-12-23 03:00:33 +0000

    filling out scrapping method

--- lib/techhhynews/news.rb
+++ lib/techhhynews/news.rb
@@ -1,16 +1,27 @@
 class News
-  attr_accessor :url, :title, :summary
+  attr_accessor :author, :title, :summary
   @@all = []
 
-  def initialize(title=nil, url=nil, summary=nil)
+  def initialize(title=nil, who=nil, summary=nil)
     @title = title
-    @url = url
+    @who = who
     @summary = summary
-    @@all < self
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def self.scrap
     doc = Nokogiri::HTML(open("https://www.google.com/search?q=technology+news&source=lnms&tbm=nws&sa=X&ved=0ahUKEwjwz9LNqpzYAhUk8IMKHTI-A5gQ_AUICigB&biw=1536&bih=759"))
+    page = doc.css("div.NId")
+    search_title=page.css("div h3")
+    search_who=page.css("div span")
+    search_summary=page.css("div div.st")
     binding.pry
+    self.all << {title: search_title, who: search_who, summary: search_summary}
   end
+
+
 end
